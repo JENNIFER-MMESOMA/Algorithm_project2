79c7d90e80444850ba3b7532c9100ffec4ea4679
JENNIFER-MMESOMA <adammesomachi@gmail.com>
2024-07-11 15:02:06 -0700

    Algorithm

---
+++ index.js
@@ -0,0 +1,25 @@
+function insertionSort(arr) {
+    const n = arr.length;
+    
+    for (let i = 1; i < n; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        
+        // Move elements of arr[0..i-1], that are greater than current,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        
+        arr[j + 1] = current;
+    }
+    
+    return arr;
+}
+
+let arr = [12, 11, 13, 5, 6];
+console.log("Original Array:", arr);
+
+arr = insertionSort(arr);
+console.log("Sorted Array:", arr);
